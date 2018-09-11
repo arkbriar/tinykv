4fe979cd4f38e296db6273bc88528b178f3367ec
arkbriar <arkbriar@gmail.com>
2018-09-11 17:52:17 +0800

    trying to implement epoch protection framework from FASTER

--- faster/epoch.go
+++ faster/epoch.go
@@ -19,3 +19,48 @@
 //  THE SOFTWARE.
 
 package faster
+
+import "math"
+
+const CacheLineSize = 64
+const MaxNumThreads = 64
+
+type epochEntryValue struct {
+	localCurrentEpoch uint64
+	reentrant         uint32
+}
+
+type epochEntry struct {
+	epochEntryValue
+	// padding to make sure this entry is not shared between cache lines
+	_ [CacheLineSize - 12]byte
+}
+
+const (
+	epochFree   = math.MaxUint64
+	epochLocked = math.MaxUint64 - 1
+)
+
+type EpochCallbackFunc func()
+
+type EpochAction struct {
+	epoch    uint64
+	callback EpochCallbackFunc
+}
+
+const (
+	InvalidIndex  = uint32(0)
+	Unprotected   = uint32(0)
+	TableSize     = MaxNumThreads
+	DrainListSize = 256
+)
+
+type LightEpoch struct {
+	currentEpoch       uint64
+	safeToReclaimEpoch uint64
+
+	table []epochEntry
+
+	drainCount uint32
+	drainList  [DrainListSize]EpochAction
+}
